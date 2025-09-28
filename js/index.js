8bd5ebf4e4f86f855ee7360839ffcfca50fdb9f7
Rosa C <rcardenalmilan@outlook.es>
2025-09-28 12:27:41 +0200

    configuracion de prettier, node y mejoras en el readme

--- js/index.js
+++ js/index.js
@@ -1,37 +1,40 @@
-const btnTop = document.querySelector(".top");
+const btnTop = document.querySelector('.top');
 
 // Mostrar u ocultar el botón al hacer scroll
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    btnTop.style.display = "block";
-  } else {
-    btnTop.style.display = "none";
-  }
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 50) {
+		btnTop.style.display = 'block';
+	} else {
+		btnTop.style.display = 'none';
+	}
 });
-btnTop.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+btnTop.addEventListener('click', () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	});
 });
 const hamburger = document.getElementById('hamburger');
-  const navMenu = document.getElementById('nav-menu');
+const navMenu = document.getElementById('nav-menu');
 
-  hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
-   const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-        // Puedes descomentar esto si quieres que se anime solo una vez:
-        // observer.unobserve(entry.target);
-      }
-    });
-  }, {
-    threshold: 0.1  // Empieza la animación cuando se ve el 10% de la sección
-  });
+hamburger.addEventListener('click', () => {
+	navMenu.classList.toggle('active');
+});
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('visible');
+				// Puedes descomentar esto si quieres que se anime solo una vez:
+				// observer.unobserve(entry.target);
+			}
+		});
+	},
+	{
+		threshold: 0.1 // Empieza la animación cuando se ve el 10% de la sección
+	}
+);
 
-  document.querySelectorAll('.pantalla-completa').forEach(section => {
-    observer.observe(section);
-  });
+document.querySelectorAll('.pantalla-completa').forEach((section) => {
+	observer.observe(section);
+});
